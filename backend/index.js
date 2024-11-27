baa46565bb045ba123e1de0bc5ab4ec7abdc8dd2
Anand Merugu <120920385+meruguanand109@users.noreply.github.com>
2024-11-27 11:57:33 +0530

    Update index.js url

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.listen(port,()=>{
 })
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://192.168.29.221:3000")
+    res.setHeader("Access-Control-Allow-Origin","https://achat-three.vercel.app")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
